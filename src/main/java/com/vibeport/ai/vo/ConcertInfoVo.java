a425b75a08e64c97799b505f7b93e4860e3833b5
romano1994 <fhaksh0369@gmail.com>
2025-12-18 14:55:10 +0900

    fix: ConcertVo.java를 ConcertInfoVo.java로 대체

---
+++ src/main/java/com/vibeport/ai/vo/ConcertInfoVo.java
@@ -0,0 +1,20 @@
+package com.vibeport.ai.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ConcertInfoVo {
+    private int concertNo;
+    private String artistNm;
+    private int concertYear;
+    private int concertMonth;
+    private int concertDate;
+    private String concertTime;
+    private String venue;
+    private String tctSite;
+    private String tctOpenAt;
+    private String emailYn;
+    private LocalDateTime regTime;
+}
