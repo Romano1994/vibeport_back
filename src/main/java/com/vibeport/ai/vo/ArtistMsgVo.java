3e3c5faa4f442207a97d533ca8d7625dbbc81f9d
romano1994 <fhaksh0369@gmail.com>
2026-01-15 14:19:22 +0900

    feat: ArtistMsgVo에 칼럼 추가

--- src/main/java/com/vibeport/ai/vo/ArtistMsgVo.java
+++ src/main/java/com/vibeport/ai/vo/ArtistMsgVo.java
@@ -1,9 +1,15 @@
 package com.vibeport.ai.vo;
 
 import lombok.Data;
+import software.amazon.awssdk.core.pagination.sync.PaginatedResponsesIterator;
 
 @Data
 public class ArtistMsgVo {
+    private int concertYear;
+    private int concertMonth;
+    private int concertDate;
+    private String artistNmKor;
     private String subject;
     private String content;
+    private String sendYn;
 }
